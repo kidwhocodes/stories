86019c2658abd1aa4943b70052215652c0cdb69e
kidwhocodes <sanjithsen36@gmail.com>
2021-10-10 08:38:31 -0700

    First cut

---
+++ landlady.py
@@ -0,0 +1,12 @@
+print("LANDLADY SURVIVAL. This game contains 0 graphics because I have no idea how to use them")
+print("This story picks up from the end of the short story, ,The Landlady, where the main character Billy Weaver is just about to take a sip of tea. Upon his first sip of tea, Billy tastes a light scent of bitter almonds.") 
+Awn1 = input("Here are your options. 1.) Drink the full cup of tea. 2.) Spit the sip of tea at the Landlady and throw the cup to the floor. 3.) Tell the Landlady you are not in the mood for tea and try to distract her with small talk.")
+
+if Awn1 == 1:
+    print("You drink the full cup of tea. A few minutes later, as you are heading up to your room, you fall to the ground. You never wake up. KO")
+elif Awn1 == 2:
+    print("You spit the tea at the Landlady and throw the cup to the floor. You yell at the lady for trying to poison you and just as you turn around, you hear the cock of a rifle...")
+elif Awn1 == 3:
+    print("After tasting the tea, you sit the cup of tea on the table (after spitting the sip you tasted back in) and tell the Landlady you are not in the mood for tea. After engaging in small talk, you walk upstairs. You have to escape...")
+else:
+    print("Enter Valid Answer")
